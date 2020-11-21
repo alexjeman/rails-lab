ea708637622651f2f0570d15574c13ef6dd0f11a
alexjeman <alexandrujeman.ja@gmail.com>
2020-11-22 01:22:34 +0200

    Create article

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -16,4 +16,14 @@ class ArticlesController < ApplicationController
       format.json { render json: @article, status: :ok }
     end
   end
+
+  def new
+
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    redirect_to @article
+  end
 end
