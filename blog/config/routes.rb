a18dfa17ea1140582f4ccdbd080fb22860da8c03
alexjeman <alexandrujeman.ja@gmail.com>
2020-11-28 17:16:26 +0200

    command: rails g controller home index, build homepage

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :articles, only: %i[index show new create edit update destroy]
 end
