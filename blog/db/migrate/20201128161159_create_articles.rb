2961c4cd313f51e81854f17a016dea7adb5fbcb1
alexjeman <alexandrujeman.ja@gmail.com>
2020-11-28 18:24:54 +0200

    command: rails g scaffold Article title:string description:text user:references --force, remove old migrations

--- blog/db/migrate/20201128161159_create_articles.rb
+++ blog/db/migrate/20201128161159_create_articles.rb
@@ -2,7 +2,10 @@ class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
       t.string :title
-      t.string :description
+      t.text :description
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
     end
   end
 end
