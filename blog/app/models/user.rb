e9bdb1d0be203ed1367eea07f4ae5751696ca247
alexjeman <alexandrujeman.ja@gmail.com>
2020-11-28 14:54:24 +0200

    User associations

--- blog/app/models/user.rb
+++ blog/app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false },
                     length: { maximum: 50 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+
+  has_many :articles
 end
