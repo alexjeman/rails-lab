92a540aa4d14f2dc476026e0fdfcfd0b43713aa1
alexjeman <alexandrujeman.ja@gmail.com>
2020-11-29 10:28:01 +0200

    Edit user and show user with gravatar

--- blog/app/controllers/users_controller.rb
+++ blog/app/controllers/users_controller.rb
@@ -1,6 +1,26 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
   def new
     @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(params.require(:user).permit(:username, :email, :password))
+      flash[:notice] = 'Account updated!'
+      render 'edit', notice: 'Account information was successfully updated.'
+    else
+      render 'edit'
+    end
   end
 
   def create
